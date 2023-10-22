2672de539088caea91768a51391dda9096a533d2
Chr1zM <ch.me010103@gmail.com>
2023-10-22 23:40:04 +0200

    TODO RockHead Animation

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -18,6 +18,9 @@ public class FallingBlock : MonoBehaviour
     [SerializeField] private float returnSpeed = 4f;
     [SerializeField] private LayerMask resetGround;
 
+    private enum BlockState { Idle, Hit, Ready }
+    private BlockState blockState = BlockState.Idle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,8 +36,20 @@ public class FallingBlock : MonoBehaviour
         if (falling)
             rb.velocity = new Vector2(0, -fallSpeed);
         else
+        {
             transform.position = Vector2.Lerp(transform.position, initialPosition, Time.deltaTime * returnSpeed);
-        
+
+        }
+
+        if (isOnInitPosition())
+        {
+            blockState = BlockState.Ready;
+        }
+        else if (blockState != BlockState.Hit)
+        {
+            blockState = BlockState.Idle;
+        }
+        animator.SetInteger("blockState", (int)blockState);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -42,10 +57,14 @@ public class FallingBlock : MonoBehaviour
         if (Physics2D.BoxCast(collider2d.bounds.center, collider2d.bounds.size, 0f, Vector2.down, 0.01f, resetGround))
         {
             // TODO Animation on Hit
-            animator.SetTrigger("hit");
+            Debug.Log("RockHead: Hit the Ground");
+            Debug.Log("Collision detected. Setting blockState to Hit.");
+
+            blockState = BlockState.Hit;
+            animator.SetInteger("blockState", (int)blockState);
             ResetBlock();
         }
-            
+
     }
 
     private bool isOnInitPosition()
@@ -53,8 +72,6 @@ public class FallingBlock : MonoBehaviour
         float positionDifferenceX = Mathf.Abs(transform.position.x - initialPosition.x);
         float positionDifferenceY = Mathf.Abs(transform.position.y - initialPosition.y);
 
-        // TODO Animation "Blink" for Player Info that the Block is now "Activated" again
-        animator.SetTrigger("reset");
         return positionDifferenceX < 0.1f && positionDifferenceY < 0.1f;
     }
 
@@ -71,5 +88,7 @@ public class FallingBlock : MonoBehaviour
     {
         falling = false;
         rb.velocity = Vector2.zero;
+        blockState = BlockState.Idle;
+        animator.SetInteger("blockState", (int)blockState);
     }
 }
