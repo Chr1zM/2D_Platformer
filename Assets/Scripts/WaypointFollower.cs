dc10d6b8ac4693e68806ba775fa9ff81cd90d489
Chr1zM <ch.me010103@gmail.com>
2023-11-14 21:03:52 +0100

    Add WaypointFollower dynamic Chains

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -9,8 +9,38 @@ public class WaypointFollower : MonoBehaviour
 
     [SerializeField] private float speed = 2f;
 
-    // Update is called once per frame
+    [SerializeField] private GameObject chainPrefab;
+    private List<GameObject> chainLinks = new List<GameObject>();
+
+
+    private void Start()
+    {
+        InitializeChains();
+    }
+
     private void Update()
+    {
+        MoveToNextWaypoint();
+    }
+
+    private void InitializeChains()
+    {
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            float distance = Vector3.Distance(waypoints[i].transform.position, waypoints[i + 1].transform.position);
+            int numberOfChains = Mathf.RoundToInt(distance) + 2;
+
+            for (int j = 0; j < numberOfChains; j++)
+            {
+                float t = (float)(j + 1) / (numberOfChains + 1);
+                Vector3 chainPosition = Vector3.Lerp(waypoints[i].transform.position, waypoints[i + 1].transform.position, t);
+                GameObject chainLink = Instantiate(chainPrefab, chainPosition, Quaternion.identity);
+                chainLinks.Add(chainLink);
+            }
+        }
+    }
+
+    private void MoveToNextWaypoint()
     {
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f)
         {
