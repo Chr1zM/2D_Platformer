717f48bc39e379b9286eae7f90e7d480ee33fdc6
Chr1zM <ch.me010103@gmail.com>
2023-11-22 21:01:55 +0100

    WIP Trampoline functionality

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField] private float bounceForce = 30f;
+    [SerializeField] private float cooldownTime = 1f;
+
+    private bool canBounce = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (canBounce && collision.gameObject.CompareTag("Player"))
+        {
+            BouncePlayer(collision.gameObject);
+        }
+    }
+
+    private void BouncePlayer(GameObject player)
+    {
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        Vector2 bounceDirection = CalculateBounceDirection(player.transform.position);
+
+        playerRigidbody.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
+
+        canBounce = false;
+        StartCoroutine(Cooldown());
+    }
+
+    private Vector2 CalculateBounceDirection(Vector2 playerPosition)
+    {
+        return (playerPosition - (Vector2)transform.position).normalized;
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldownTime);
+        canBounce = true;
+    }
+}
